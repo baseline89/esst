b01266131ee19bb15e5f10b7fe32926609109f15
Aaron Gawronsky <agawronsky@gmail.com>
2023-05-30 20:21:21 -0400

    ORM Chapter Additions

--- dj_esst/notes/models.py
+++ dj_esst/notes/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
 class Notes(models.Model):
-    title = models.CharField(max_length = 200)
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    notes = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
